7c5fcbcd2a88db722c9fa01e2c6739a941860b8b
Angyalffy Dávid Máté <martin.angyalffygmail.com>
2021-03-18 12:24:26 +0100

    feat: register done

--- inc/register.inc.php
+++ inc/register.inc.php
@@ -1,1 +1,27 @@
-<?php?>
+<?php
+include "connection.php";
+
+if (isset($_POST['user']) &&  isset($_POST['psw'])){
+
+    $user = $_POST['user'];
+    $psw = $_POST['psw'];
+    $hash = password_hash($psw, PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users(name, password) VALUES('$user','$hash')";
+
+    $conn -> query($sql);
+
+    header("Location: ../index.php/#");
+}
+else{
+    header("Location: ../index.php/#");
+}
+
+
+
+
+
+
+
+
+?>
